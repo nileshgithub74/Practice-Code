aee4bd9928b0e0a6f7e0e6301b3c1abe0e3b7f3f
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-04-24 00:14:44 +0000

    Remove duplicate characters.

--- RemoveCharacter.java
+++ RemoveCharacter.java
@@ -5,8 +5,13 @@ public class RemoveCharacter {
         System.out.println("Enter the String");
         String str = sc.nextLine();
 
-        String replaceString = str.replaceAll("[0-9]","");
-        System.out.println(replaceString);
+        // String replaceString = str.replaceAll("[0-9]","");
+        // System.out.println(replaceString);
+
+        // String trimString = str.trim();  /// it will remove leading and back space not the space btw word.
+        // System.out.println(trimString);
+
+        System.out.println(str.replaceAll("\\s",""));
 
     }
     
