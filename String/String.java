155557164b725bd79f03751e04ff0861494d27cd
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-05-03 12:15:55 +0000

    String

---
+++ String/String.java
@@ -0,0 +1,5 @@
+import java.uitl.*;
+public class StringDemo
+{
+
+}
---
+++ String/StringDem.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class StringDem{
+    public  static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the String: ");
+        // String str = sc.nextLine();
+
+        // System.out.println("Entered String : "+str);
+        // int length = str.length();
+        // System.out.println("length"+length);
+
+        // char ch = str.charAt(4);
+        // System.out.println("character : "+ ch);
+
+        // System.out.println(str.indexOf('a'));
+
+
+
+        String str1 = "nilesh";
+        String str2 = "Nilesh";
+
+        int str = str1.compareTo(str2);
+        System.out.println(str);
+
+        
+    }
+
+}
+
+
+
