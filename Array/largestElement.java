2a6e1a5601303ba27dc2131146bee30cfed7ae07
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-22 18:08:08 +0000

    java

--- Array/largestElement.java
+++ Array/largestElement.java
@@ -1,19 +1,19 @@
-package Array;
+// package Array;
 
-public class largestElement {
-    public static void main(String[] args){
+// public class largestElement {
+//     public static void main(String[] args){
 
-        int[] arr = {10,20,30,40,5023,34};
-        int max = arr[0];
+//         int[] arr = {10,20,30,40,5023,34};
+//         int max = arr[0];
 
-        for(int i =0; i< arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
-        }
+//         for(int i =0; i< arr.length; i++){
+//             if(arr[i] > max){
+//                 max = arr[i];
+//             }
+//         }
 
-        System.out.println(max);
-    }
+//         System.out.println(max);
+//     }
 
     
-}
+// }
